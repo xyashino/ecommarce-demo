de0602e23517cd1cb8cf42fb246450c2a16c187b
Szczepan Micek <micek.szczepan.w@gmail.com>
2025-04-06 14:26:40 +0200

    init

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  singleQuote: true,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  printWidth: 80,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  tabWidth: 2
+}
+
+export default config
